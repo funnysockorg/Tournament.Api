c2cfc761f405db3949b654688a2fbdbbb5aa0556
L'gotin Bogdan <dr34mway@gmail.com>
2024-05-10 16:01:20 +0600

    Make routing for api.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,18 @@
 <?php
 
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\DiscordController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [TestController::class, 'index'])
-    ->name('api.login');
+Route::get('/home', [DiscordController::class, 'index'])
+    ->name('api.home');
+
+Route::group(['prefix' => 'discord'], function () {
+    Route::get('/login', [DiscordController::class, 'discordRedirectGetCode'])
+        ->name('api.discord.login');
+
+    Route::get('/callback', [DiscordController::class, 'discordAuth'])
+        ->name('api.discord.callback');
+
+    Route::get('/get-user-data', [DiscordController::class, 'getUserData'])
+        ->name('api.discord.get-user-data');
+});
