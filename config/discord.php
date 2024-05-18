e3c8428d89b8131ed95a4a8d83e83706ff9d67c7
L'gotin Bogdan <dr34mway@gmail.com>
2024-05-18 19:53:38 +0600

    Add client callback endpoint config param.

--- config/discord.php
+++ config/discord.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    'client_id' => env('DISCORD_CLIENT_ID'),
-    'secret_key' => env('DISCORD_SECRET_KEY'),
-    'redirect_url' => env('DISCORD_REDIRECT_URL'),
+    'client_id' => env('DISCORD_CLIENT_ID', ''),
+    'secret_key' => env('DISCORD_SECRET_KEY', ''),
+    'redirect_url' => env('DISCORD_REDIRECT_URL', ''),
+
+    'client_callback_endpoint' => env('CLIENT_CALLBACK_ENDPOINT', ''),
 ];
