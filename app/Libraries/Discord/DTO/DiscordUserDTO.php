0a1c79cfe41f6c061575e8e863141d4a3bcb0c28
L'gotin Bogdan <dr34mway@gmail.com>
2024-05-26 22:58:52 +0600

    Mini refactoring.

--- app/Libraries/Discord/DTO/DiscordUserDTO.php
+++ app/Libraries/Discord/DTO/DiscordUserDTO.php
@@ -6,32 +6,32 @@ use Illuminate\Http\Client\Response;
 
 class DiscordUserDTO
 {
-    private string $userId;
-    private string $userName;
+    private string $discordId;
+    private string $name;
 
     public function __construct(Response $response)
     {
         $userData = json_decode($response->body());
 
         if (!empty($userData->id)) {
-            $this->userId = $userData->id;
+            $this->discordId = $userData->id;
         }
 
         if (!empty($userData->username)) {
-            $this->userName = $userData->username;
+            $this->name = $userData->username;
         }
     }
 
     public function toJsonString(): string
     {
         return json_encode([
-            'user_id' => $this->userId,
-            'user_name' => $this->userName,
+            'user_id' => $this->discordId,
+            'user_name' => $this->name,
         ]);
     }
 
     public function isEmpty(): bool
     {
-        return empty($this->userId) || empty($this->userName);
+        return empty($this->discordId) || empty($this->name);
     }
 }
